7237e99787e0d8f393ec8a003bab9514036d5756
giaphu39 <110403306+giaphu39@users.noreply.github.com>
2023-03-17 00:34:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import sklearn
 pickled_model = pickle.load(open('model.pickle', 'rb'))
 st.title('Revenue Prediction')
 x = st.number_input('Input Temperature')
-x=x.arrray(reshape(-1,1))
 if st.button('Predict'):
   st.text('Revenue Prediction')
   y_pred = pickled_model.predict(x)
